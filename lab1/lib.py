b89e4da2a80289b2661784bbe126898bcc9b3644
Giorgos Paraskevopoulos <georgepar.91@gmail.com>
2020-10-25 18:27:48 +0200

    Update lib.py

--- lab1/lib.py
+++ lab1/lib.py
@@ -2,114 +2,114 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 
 def show_sample(X, index):
-	'''Takes a dataset (e.g. X_train) and imshows the digit at the corresponding index
-	
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		index (int): index of digit to show
-	'''
-	raise NotImplementedError
+    '''Takes a dataset (e.g. X_train) and imshows the digit at the corresponding index
+
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        index (int): index of digit to show
+    '''
+    raise NotImplementedError
 
 
 def plot_digits_samples(X, y):
-	'''Takes a dataset and selects one example from each label and plots it in subplots
+    '''Takes a dataset and selects one example from each label and plots it in subplots
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-	'''
-	raise NotImplementedError
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+    '''
+    raise NotImplementedError
 
 
 def digit_mean_at_pixel(X, y, digit, pixel=(10, 10)):
-	'''Calculates the mean for all instances of a specific digit at a pixel location
+    '''Calculates the mean for all instances of a specific digit at a pixel location
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-		digit (int): The digit we need to select
-		pixels (tuple of ints): The pixels we need to select. 
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+        digit (int): The digit we need to select
+        pixels (tuple of ints): The pixels we need to select.
 
-	Returns:
-		(float): The mean value of the digits for the specified pixels
-	'''
-	raise NotImplementedError
+    Returns:
+        (float): The mean value of the digits for the specified pixels
+    '''
+    raise NotImplementedError
 
 
 
 def digit_variance_at_pixel(X, y, digit, pixel=(10, 10)):
-	'''Calculates the variance for all instances of a specific digit at a pixel location
+    '''Calculates the variance for all instances of a specific digit at a pixel location
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-		digit (int): The digit we need to select
-		pixels (tuple of ints): The pixels we need to select
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+        digit (int): The digit we need to select
+        pixels (tuple of ints): The pixels we need to select
 
-	Returns:
-		(float): The variance value of the digits for the specified pixels
-	'''
-	raise NotImplementedError
+    Returns:
+        (float): The variance value of the digits for the specified pixels
+    '''
+    raise NotImplementedError
 
 
 
 def digit_mean(X, y, digit):
-	'''Calculates the mean for all instances of a specific digit
+    '''Calculates the mean for all instances of a specific digit
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-		digit (int): The digit we need to select
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+        digit (int): The digit we need to select
 
-	Returns:
-		(np.ndarray): The mean value of the digits for every pixel
-	'''
-	raise NotImplementedError
+    Returns:
+        (np.ndarray): The mean value of the digits for every pixel
+    '''
+    raise NotImplementedError
 
 
 
 def digit_variance(X, y, digit):
-	'''Calculates the variance for all instances of a specific digit
+    '''Calculates the variance for all instances of a specific digit
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-		digit (int): The digit we need to select
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+        digit (int): The digit we need to select
 
-	Returns:
-		(np.ndarray): The variance value of the digits for every pixel
-	'''
-	raise NotImplementedError
+    Returns:
+        (np.ndarray): The variance value of the digits for every pixel
+    '''
+    raise NotImplementedError
 
 
 def euclidean_distance(s, m):
-	'''Calculates the euclidean distance between a sample s and a mean template m
+    '''Calculates the euclidean distance between a sample s and a mean template m
 
-	Args:
-		s (np.ndarray): Sample (nfeatures)
-		m (np.ndarray): Template (nfeatures)
+    Args:
+        s (np.ndarray): Sample (nfeatures)
+        m (np.ndarray): Template (nfeatures)
 
-	Returns:
-		(float) The Euclidean distance between s and m
-	'''
-	raise NotImplementedError
+    Returns:
+        (float) The Euclidean distance between s and m
+    '''
+    raise NotImplementedError
 
 
 def euclidean_distance_classifier(X, X_mean):
-	'''Classifiece based on the euclidean distance between samples in X and template vectors in X_mean
+    '''Classifiece based on the euclidean distance between samples in X and template vectors in X_mean
 
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		X_mean (np.ndarray): Digits data (n_classes x nfeatures)
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        X_mean (np.ndarray): Digits data (n_classes x nfeatures)
 
-	Returns:
-		(np.ndarray) predictions (nsamples)
-	'''
-	raise NotImplementedError
+    Returns:
+        (np.ndarray) predictions (nsamples)
+    '''
+    raise NotImplementedError
 
 
 
-class EuclideanDistanceClassifier(BaseEstimator, ClassifierMixin):  
+class EuclideanDistanceClassifier(BaseEstimator, ClassifierMixin):
     """Classify samples based on the distance from the mean feature value"""
 
     def __init__(self):
@@ -119,13 +119,13 @@ class EuclideanDistanceClassifier(BaseEstimator, ClassifierMixin):
     def fit(self, X, y):
         """
         This should fit classifier. All the "work" should be done here.
-        
-        Calculates self.X_mean_ based on the mean 
+
+        Calculates self.X_mean_ based on the mean
         feature values in X for each class.
-        
-        self.X_mean_ becomes a numpy.ndarray of shape 
+
+        self.X_mean_ becomes a numpy.ndarray of shape
         (n_classes, n_features)
-        
+
         fit always returns self.
         """
         raise NotImplementedError
@@ -138,7 +138,7 @@ class EuclideanDistanceClassifier(BaseEstimator, ClassifierMixin):
         euclidean distance from self.X_mean_
         """
         raise NotImplementedError
-    
+
     def score(self, X, y):
         """
         Return accuracy score on the predictions
@@ -148,51 +148,51 @@ class EuclideanDistanceClassifier(BaseEstimator, ClassifierMixin):
 
 
 def evaluate_classifier(clf, X, y, folds=5):
-	"""Returns the 5-fold accuracy for classifier clf on X and y
+    """Returns the 5-fold accuracy for classifier clf on X and y
 
-	Args:
-		clf (sklearn.base.BaseEstimator): classifier
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
+    Args:
+        clf (sklearn.base.BaseEstimator): classifier
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
 
-	Returns:
-		(float): The 5-fold classification score (accuracy)
-	"""
-	raise NotImplementedError
+    Returns:
+        (float): The 5-fold classification score (accuracy)
+    """
+    raise NotImplementedError
 
-	
+
 class calculate_priors(X, y):
-	"""Return the a-priori probabilities for every class
-	
-	Args:
-		X (np.ndarray): Digits data (nsamples x nfeatures)
-		y (np.ndarray): Labels for dataset (nsamples)
-		
-	Returns:
-		(np.ndarray): (n_classes) Prior probabilities for every class
-	"""
-	raise NotImplementedError
-	
+    """Return the a-priori probabilities for every class
+
+    Args:
+        X (np.ndarray): Digits data (nsamples x nfeatures)
+        y (np.ndarray): Labels for dataset (nsamples)
+
+    Returns:
+        (np.ndarray): (n_classes) Prior probabilities for every class
+    """
+    raise NotImplementedError
 
 
-class CustomNBClassifier(BaseEstimator, ClassifierMixin):  
+
+class CustomNBClassifier(BaseEstimator, ClassifierMixin):
     """Custom implementation Naive Bayes classifier"""
 
     def __init__(self, use_unit_variance=False):
         self.X_mean_ = None
-	self.use_unit_variance = use_unit_variance
+    self.use_unit_variance = use_unit_variance
 
 
     def fit(self, X, y):
         """
         This should fit classifier. All the "work" should be done here.
-        
-        Calculates self.X_mean_ based on the mean 
+
+        Calculates self.X_mean_ based on the mean
         feature values in X for each class.
-        
-        self.X_mean_ becomes a numpy.ndarray of shape 
+
+        self.X_mean_ becomes a numpy.ndarray of shape
         (n_classes, n_features)
-        
+
         fit always returns self.
         """
         raise NotImplementedError
@@ -205,7 +205,7 @@ class CustomNBClassifier(BaseEstimator, ClassifierMixin):
         euclidean distance from self.X_mean_
         """
         raise NotImplementedError
-    
+
     def score(self, X, y):
         """
         Return accuracy score on the predictions
@@ -213,31 +213,28 @@ class CustomNBClassifier(BaseEstimator, ClassifierMixin):
         """
         raise NotImplementedError
 
-	
+
 class PytorchNNModel(BaseEstimator, ClassifierMixin):
-	def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         # WARNING: Make sure predict returns the expected (nsamples) numpy array not a torch tensor.
-		# TODO: initialize model, criterion and optimizer
-		self.model = ...
-		self.criterion = ...
-		self.optimizer = ...
-	    raise NotImplementedError
-        
+        # TODO: initialize model, criterion and optimizer
+        self.model = ...
+        self.criterion = ...
+        self.optimizer = ...
+        raise NotImplementedError
+
     def fit(self, X, y):
         # TODO: split X, y in train and validation set and wrap in pytorch dataloaders
         train_loader = ...
         val_loader = ...
         # TODO: Train model
         raise NotImplementedError
-    
-    def predict(self, X):    
-	    # TODO: wrap X in a test loader and evaluate
+
+    def predict(self, X):
+        # TODO: wrap X in a test loader and evaluate
         test_loader = ...
         raise NotImplementedError
-    
+
     def score(self, X, y):
         # Return accuracy score.
         raise NotImplementedError
-		     
-		     
-		     
